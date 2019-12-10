ff1471ea66498b660b1edc901793cc0519a3c84b
Aaron Andersen <anderseana@tutanota.com>
2019-12-10 15:31:16 -0600

    reworked the numbers js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,17 +1,14 @@
 import React from "react";
-import {numbers, operators, specials} from '../../../data';
+import {numbers} from '../../../data';
 import NumberButton from './NumberButton';
 
-const [numberState, setNumberState] = useState(numbers);
-
 const Numbers = () => {
-  const {setNumbDisplay} = props;
+  const [numState] = useState(numbers);
   return (
     <div>
         {
-          numbers.map((numbrz) => {
-            <NumberButton numbrz = {numbrz} key = {numbrz}
-            setNumbDisplay={setNumbDisplay}/>
+          numState.map((num, index) => {
+            <NumberButton key = {index} numState={num}/>
           })
         }
     </div>
