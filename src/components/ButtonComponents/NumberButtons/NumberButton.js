09bf438556d6ba6f09eb50bbbf0c22fd2174b962
Aaron Andersen <anderseana@tutanota.com>
2019-12-10 14:26:13 -0600

    imported and exported things and did steps 1 to 3

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {numbers, operators, specials} from '../../../data';
 
-const NumberButton = () => {
+const NumberButton = ([numbrz]) => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <div>
+      <button>numbrz</button>
+    </div>
   );
 };
+// {/* Display a button element rendering the data being passed down from the parent container on props */}
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import {numbers, operators, specials} from '../../../data';
+import NumberButton from './NumberButton';
+const [numberState, setNumberState] = useState(numbers);
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -9,11 +11,19 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+        {
+          numbers.map((numbrz) => {
+            <NumberButton> numbrz = {numbrz} key = {numbrz}</NumberButton>
+          })
+        }
     </div>
   );
 };
+export default Numbers;
+
+{/* STEP 3 - Use .map() to iterate over your array data and return a button
+       component matching the name on the provided file. Pass
+       it any props needed by the child component*/}
